5175f89baf8e84ef682c4353efd55200094e44f9
Jordan Hinks <jordanhinks0+github@gmail.com>
2025-05-10 16:35:39 +0100

    Corrected domain in const.py

--- custom_components/abc_council_bin_collection/const.py
+++ custom_components/abc_council_bin_collection/const.py
@@ -1,4 +1,4 @@
-DOMAIN = "bin_collection"
+DOMAIN = "abc_council_bin_collection"
 DEFAULT_UPDATE_INTERVAL = 96  # hours
 MIN_UPDATE_INTERVAL = 6       # hours
 EVENT_CREATION_DELAY = 30     # seconds
